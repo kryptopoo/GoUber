042144a76b81d33519c4132af350870ea899c702
kryptopoo <kryptopoo@gmail.com>
2018-12-09 11:55:20 +0700

    dev

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export const myConfig = { 
   httpProviderHost: "http://127.0.0.1:7545",
-  googleApiKey: ",
+  googleApiKey: "",
 
   currency: "Ether",
   etherWeiRate: 10 ** 18,	// 1 ether = 1000000000000000000 wei
